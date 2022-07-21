0965f24370ccdab3ec5793fdddc6e795097f35c7
Mdbshero <mdbshero@gmail.com>
2022-07-21 15:36:33 -0500

    Game is now a two player game. Added total scores for each player. Added changing HTML for each player based on who's turn it is

--- farkle.js
+++ farkle.js
@@ -3,15 +3,15 @@ var diceClicked = []; //Clicked die values
 var checkedScore = 0; // Checked score
 var uncheckedScore = 0; //Checks unclicked dice for possible points.
 var roundScore = 0; // Round Score
-var totalScore = 0; //Total score
+var totalScoreOne = 0; //Total score
+var totalScoreTwo = 0;
 var dieValues = []; //pulls die values into array
 var gameRound = 1; //Keeps track of game round
-var rollBTN = document.getElementById('roll'); //roll button
-var checkBTN = document.getElementById('check');//Check score button
-var bankBTN = document.getElementById('bank');//Bank Score button
-var endBTN = document.getElementById('end');//End round button
-
-
+var player = 1; //Current Player
+var rollBTN = document.getElementById("roll"); //roll button
+var checkBTN = document.getElementById("check"); //Check score button
+var bankBTN = document.getElementById("bank"); //Bank Score button
+var endBTN = document.getElementById("end"); //End round button
 
 function initializeDice() {
   for (i = 0; i < 6; i++) {
@@ -26,20 +26,27 @@ function initializeDice() {
 function rollDice() {
   dieValues = [];
   uncheckedScore = 0;
+  document.getElementById("header").innerHTML = "Player "+ player + " Round: " + gameRound;
   for (var i = 0; i < 6; i++) {
     if (diceArr[i].clicked === 0) {
       diceArr[i].value = Math.floor(Math.random() * 6) + 1;
       dieValues.push(diceArr[i].value);
     }
   }
-  checkBTN.removeAttribute('disabled')
-  checkScoreUnclicked(dieValues)
+  checkBTN.removeAttribute("disabled");
+  checkScoreUnclicked(dieValues);
   if (uncheckedScore === 0) {
     roundScore = 0;
-    document.getElementById("round score").innerHTML = "R: " + roundScore;
-    bankBTN.setAttribute('disabled', '')
-    checkBTN.setAttribute('disabled', '')
-    document.getElementById('header').innerHTML = "You Farkled! Please Click End Round"
+    if (player === 1) {
+      document.getElementById("round score 1").innerHTML = "R: " + roundScore;
+      player = 2;
+    } else {
+      document.getElementById("round score 2").innerHTML = "R: " + roundScore;
+    }
+    bankBTN.setAttribute("disabled", "");
+    checkBTN.setAttribute("disabled", "");
+    document.getElementById("header").innerHTML =
+      "You Farkled! Please Click End Round";
   }
   updateDiceImg();
 }
@@ -61,7 +68,11 @@ function diceClick(img) {
     diceClicked.push(diceArr[i]); //Creates array filled with clicked dice for scoring.
   } else {
     checkedScore = 0; // resets score so that gameLogic function can recalculate score with new clicked array.
-    document.getElementById("checked score").innerHTML = checkedScore;
+    if (player === 1){
+      document.getElementById("checked score 1").innerHTML = checkedScore;
+    } else {
+      document.getElementById("checked score 2").innerHTML = checkedScore;
+    }
     diceArr[i].clicked = 0; //Fixed to it actually sets .clicked to 1
     diceClicked = diceClicked.filter((die) => die.id !== diceArr[i].id); //Removed dice from array that are not clicked.
   }
@@ -210,7 +221,7 @@ function uncheckScoreSix(dice) {
 
 //Calculates score when Check Score is clicked.
 function checkScoreAll(dice) {
-  console.log(diceClicked)
+  console.log(diceClicked);
   checkedScore = 0;
   dice = diceClicked;
   dice = dice.map((dice) => dice.value);
@@ -220,8 +231,12 @@ function checkScoreAll(dice) {
   checkScoreFour(dice);
   checkScoreFive(dice);
   checkScoreSix(dice);
-  document.getElementById("checked score").innerHTML = 'C: ' + checkedScore;
-  bankBTN.removeAttribute('disabled')
+  if (player === 1) {
+    document.getElementById("checked score 1").innerHTML = "C: " + checkedScore;
+  } else {
+    document.getElementById("checked score 2").innerHTML = "C: " + checkedScore;
+  }
+  bankBTN.removeAttribute("disabled");
 }
 //Calculates score of unchecked dice to make sure user did not lose turn
 function checkScoreUnclicked(dice) {
@@ -232,7 +247,13 @@ function checkScoreUnclicked(dice) {
   uncheckScoreFour(dice);
   uncheckScoreFive(dice);
   uncheckScoreSix(dice);
-  document.getElementById("unchecked score").innerHTML = 'UN: ' + uncheckedScore;
+  if (player === 1) {
+    document.getElementById("unchecked score 1").innerHTML =
+    "UN: " + uncheckedScore;
+  } else {
+    document.getElementById("unchecked score 2").innerHTML =
+    "UN: " + uncheckedScore;
+  }
 }
 //Banks the round score and sets the diceClicked to empty so you cannot use dice you have already scored.
 function bankRoundScore() {
@@ -240,25 +261,52 @@ function bankRoundScore() {
   checkedScore = 0;
   uncheckedScore = 0;
   diceClicked = [];
-  document.getElementById("checked score").innerHTML = "C: " + checkedScore;
-  document.getElementById("round score").innerHTML = "R: " + roundScore;
-  document.getElementById("unchecked score").innerHTML = "UC: " + uncheckedScore;
+  if (player === 1) {
+    document.getElementById("checked score 1").innerHTML = "C: " + checkedScore;
+    document.getElementById("round score 1").innerHTML = "R: " + roundScore;
+    document.getElementById("unchecked score 1").innerHTML =
+      "UC: " + uncheckedScore;
+  } else {
+    document.getElementById("checked score 2").innerHTML = "C: " + checkedScore;
+    document.getElementById("round score 2").innerHTML = "R: " + roundScore;
+    document.getElementById("unchecked score 2").innerHTML =
+      "UC: " + uncheckedScore;
+  }
 }
 
 //Will eventually end round and bank score but for now just banks total score. Resets images to initial images and resets transparency.
 function endRoundScore() {
-  totalScore += roundScore;
-  checkedScore = 0;
-  roundScore = 0;
-  uncheckedScore = 0;
-  gameRound++;
-  document.getElementById("checked score").innerHTML = "C: " + checkedScore;
-  document.getElementById("round score").innerHTML = "R: " + roundScore;
-  document.getElementById("total score").innerHTML = "T: " + totalScore;
-  document.getElementById("unchecked score").innerHTML = "UN: " + uncheckedScore;
-  bankBTN.setAttribute('disabled', '')
-  checkBTN.setAttribute('disabled', '')
-  document.getElementById('header').innerHTML = "Round: " + gameRound
+  if (player === 1) {
+    totalScoreOne += roundScore;
+    checkedScore = 0;
+    roundScore = 0;
+    uncheckedScore = 0;
+    console.log(totalScoreOne)
+    document.getElementById("total score 1").innerHTML = "T: " + totalScoreOne;
+    document.getElementById("header").innerHTML =
+      "Player 2 Round: " + gameRound;
+    document.getElementById("checked score 1").innerHTML = "C: " + checkedScore;
+    document.getElementById("round score 1").innerHTML = "R: " + roundScore;
+    document.getElementById("unchecked score 1").innerHTML =
+      "UN: " + uncheckedScore;
+      player = 2;
+  } else {
+    totalScoreTwo += roundScore;
+    checkedScore = 0;
+    roundScore = 0;
+    uncheckedScore = 0;
+    gameRound++;
+    document.getElementById("total score 2").innerHTML = "T: " + totalScoreTwo;
+    document.getElementById("checked score 2").innerHTML = "C: " + checkedScore;
+    document.getElementById("round score 2").innerHTML = "R: " + roundScore;
+    document.getElementById("unchecked score 2").innerHTML =
+      "UN: " + uncheckedScore;
+      document.getElementById("header").innerHTML =
+      "Player 1 Round: " + gameRound;
+    player = 1
+  }
+  bankBTN.setAttribute("disabled", "");
+  checkBTN.setAttribute("disabled", "");
   initializeDice();
   updateDiceImg();
   let imgs = document.querySelectorAll("img");
@@ -266,5 +314,7 @@ function endRoundScore() {
 }
 
 //Still need to add logic that ends users turn if they cannot score with dice shown. Create array of unclicked dice that scores the potential of the unlicked dice. If zero, ends users turn and gives them zero points.
+//done ^
 //Add another player
+//Current ^
 //Update CSS
